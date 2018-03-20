75c88e8588bdb0ef3a2a9e0af3e9d9420b70b7c2
Fernando Cagua <efcaguab@gmail.com>
2018-03-20 19:13:31 +1300

    Fit tide models

--- code/tides.R
+++ code/tides.R
@@ -5,18 +5,34 @@
 #' @return a data frame with columns date, value and state
 #'
 read_tide <- function(x){
+  # read files
   data <- read_csv(x, skip = 31, 
                    col_names = c("date", "time", "value", "state"), 
                    col_types = "ccdc") %>%
+    # extract date from data
     mutate(date = paste(date, time), 
            date = as.POSIXct(date, 
                              tz = "Australia/Sydney", 
                              format = "%d/%m/%Y %H:%M"), 
+           # make sure value is treated as numeric
            value = as.numeric(value)) %>%
     select(-time) 
   
+  # extract some metadata about the sites
   metadata <- read_csv(x, skip = 20, n_max = 8, col_names = c("var", "value"), 
                        col_types = "cc")
   
   list(data = data, metadata = metadata)
 }
+
+fit_tide <- function(x){
+  # make sure there is not NA date values
+  x$data <- x$data %>%
+    filter(!is.na(date))
+  
+  # predict tides with as many harmonics as possible
+  t <- ftide(x$data$value, x$data$date, TideHarmonics::hc114)
+  
+  list(fit = t, metadata = x$metadata)
+}
+
--- main.R
+++ main.R
@@ -3,26 +3,37 @@ library(dplyr)
 library(tools)
 library(readr)
 library(stringr)
+library(TideHarmonics)
 
 # load functions
 functions_folder <- './code'
 list_files_with_exts(functions_folder, 'R') %>%
   lapply(source) %>% invisible()
 
+# list raw files with tide data
 tide_files <- list_files_with_exts('./data/tides', 'csv')
 tide_names <- basename(tide_files) %>% file_path_sans_ext() %>% 
   str_extract("^([A-Z])\\w+")
 tide_files <- paste0("'", tide_files, "'")
 
-tides_plan <- drake_plan(
+# make a plan to read the data
+tides_read <- drake_plan(
   raw = read_tide(FILE)
 ) %>%
   evaluate_plan(rules = list(FILE = tide_files), expand = T) %>%
   mutate(target = paste("raw", tide_names, sep = "_"))
 
-plan <- rbind(tides_plan)
+# make a plan to fit and predict tide data
+tides_fit <- drake_plan(
+  model = fit_tide(raw_NAME)
+) %>%
+  evaluate_plan(rules = list(NAME = tide_names))
 
+# gather plan
+plan <- rbind(tides_read, tides_fit)
 config <- drake_config(plan)
 vis_drake_graph(config)
-make(tides_plan)
+
+# run plan
+make(plan)
 
